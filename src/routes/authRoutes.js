2e1e3d20203c55c24461e59861858e40963ef1f6
Bruna <139784873+brunasiaovn@users.noreply.github.com>
2024-02-25 09:09:34 -0300

    vou começar de novo

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController.js";
-
-const authRouter = Router();
-
-authRouter.post("/signup", authController.signup);
-authRouter.post("/signin", authController.signin);
-
-export default authRouter
