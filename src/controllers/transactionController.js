0d66d3107e971b18cefe54205136bd6bd2e0ccd2
Bruna <bruna.dev@hotmail.com>
2024-02-29 07:45:20 -0300

    Buscando todas as transações por usuário

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -12,4 +12,15 @@ async function create(req, res) {
   }
 }
 
-export default { create };
+async function findAllByUser(req, res) {
+  const { _id: id } = res.locals.user;
+
+  try {
+    const transactions = await transactionService.findAllByUser(id)
+    return res.send(transactions);
+  }  catch (err) {
+    return res.status(404).send(err.message);
+  }
+}
+
+export default { create, findAllByUser };
--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -2,8 +2,12 @@ import transactionRepository from "../repositories/transactionRepository.js";
 
 async function create(body, id) {
     if (!id) throw new Error("User id is required");
-
     return await transactionRepository.create({ ...body, userId: id });
 }
 
-export default { create };
+async function findAllByUser(id) {
+    if (!id) throw new Error("User id is required");
+    return await transactionRepository.findAllByUser(id);
+}
+
+export default { create, findAllByUser };
