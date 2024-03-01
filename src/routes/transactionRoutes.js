ad7f728462cbde00474301c93a7d00c799560a1c
Bruna <bruna.dev@hotmail.com>
2024-02-29 23:06:20 -0300

    Validação de schemas com Joi

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import transactionController from "../controllers/transactionController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { validationSchemaMiddleware } from "../middlewares/validationSchemaMiddleware.js";
+import { CreateTransaction } from "../schemas/validation/CreateTransaction.js";
 
 const transactionRouter = Router();
 
@@ -8,12 +10,11 @@ transactionRouter.use(authMiddleware)
 
 transactionRouter.post(
     "/transactions",
+    validationSchemaMiddleware(CreateTransaction),
     transactionController.create
 );
 
-transactionRouter.get(
-    '/transactions',
-    transactionController.findAllByUser
+transactionRouter.get("/transactions", transactionController.findAllByUser,
 );
 
 export default transactionRouter;
